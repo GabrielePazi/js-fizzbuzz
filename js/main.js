7a0a245417e78d7275bde1a776e6d2c7b3fcc1c1
GabrielePazi <pazigabriele@gmail.com>
2023-07-04 16:47:13 +0200

    1.2 bonus 1 e 2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
+const fizzBuzzContainer = document.querySelector(".fizz-buzz-container");
+
 for (let i = 1; i <= 100; i++) {
+
+  //crea singole celle
+  let singleSquare = document.createElement("div")
+
+  //aggiunge la classe square indipendentemente dal contenuto
+  singleSquare.classList.add('square')
+
   if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz")
+
+    //assegna classe con colore e il contenuto FizzBuzz
+    singleSquare.classList.add('bg-danger')
+    singleSquare.innerHTML = "FizzBuzz"
+
+    //appende la singola cella al padre se no non si vede
+    fizzBuzzContainer.append(singleSquare)
   } else {
+
     if (i % 3 == 0) {
-      console.log("Fizz")
+
+      //assegna classe con colore e il contenuto Fizz
+      singleSquare.classList.add('bg-success')
+      singleSquare.innerHTML = "Fizz"
+
+      fizzBuzzContainer.append(singleSquare)
     } else if (i % 5 == 0) {
-      console.log("Buzz")
+
+      //assegna classe con colore e il contenuto Buzz      
+      singleSquare.classList.add('bg-warning')
+      singleSquare.innerHTML = "Buzz"
+
+      fizzBuzzContainer.append(singleSquare)
     } else {
-      console.log(i)
+
+      //assegna classe con colore e il valore dell'indice      
+      singleSquare.classList.add('bg-primary')
+      singleSquare.innerHTML = i.toString()
+
+      fizzBuzzContainer.append(singleSquare)
     }
   }
 }
