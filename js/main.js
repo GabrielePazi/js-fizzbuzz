a8f658c7af8e2f63984900f41606f7e786f25f7d
GabrielePazi <pazigabriele@gmail.com>
2023-07-04 16:59:23 +0200

    1.2.1 modifiche minori js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const fizzBuzzContainer = document.querySelector(".fizz-buzz-container");
 
 for (let i = 1; i <= 100; i++) {
-
   //crea singole celle
   let singleSquare = document.createElement("div")
 
@@ -9,36 +8,24 @@ for (let i = 1; i <= 100; i++) {
   singleSquare.classList.add('square')
 
   if (i % 3 == 0 && i % 5 == 0) {
-
     //assegna classe con colore e il contenuto FizzBuzz
     singleSquare.classList.add('bg-danger')
     singleSquare.innerHTML = "FizzBuzz"
-
-    //appende la singola cella al padre se no non si vede
-    fizzBuzzContainer.append(singleSquare)
   } else {
-
     if (i % 3 == 0) {
-
       //assegna classe con colore e il contenuto Fizz
       singleSquare.classList.add('bg-success')
       singleSquare.innerHTML = "Fizz"
-
-      fizzBuzzContainer.append(singleSquare)
     } else if (i % 5 == 0) {
-
       //assegna classe con colore e il contenuto Buzz      
       singleSquare.classList.add('bg-warning')
       singleSquare.innerHTML = "Buzz"
-
-      fizzBuzzContainer.append(singleSquare)
     } else {
-
       //assegna classe con colore e il valore dell'indice      
       singleSquare.classList.add('bg-primary')
       singleSquare.innerHTML = i.toString()
-
-      fizzBuzzContainer.append(singleSquare)
     }
   }
+  //appende la singola cella al padre se no non si vede
+  fizzBuzzContainer.append(singleSquare)
 }
